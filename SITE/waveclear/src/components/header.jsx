9f84547ba98e9a48f07a89976f6d025a52ee8d5e
dagalera <dagalera.dev@gmail.com>
2024-05-29 20:17:23 -0300

    add responsive nav

--- SITE/waveclear/src/components/header.jsx
+++ SITE/waveclear/src/components/header.jsx
@@ -1,12 +1,22 @@
 import Navbar from "./navbar";
 import fullLogo from '../assets/full-logo.png'
+import logo from '../assets/logo.png'
+import name from '../assets/name.png'
 
 function Header() {
     return (
         <>
-            <div className="flex justify-between items-center bg-nav-bg">
-                <img className="w-20 h-20 " src={fullLogo} alt="logo wave.clear"/>
+            <div className="flex justify-between items-center bg-nav-bg md:flex md:flex-col md:h-screen md:w-24 md:justify-normal md:pb-0 ">
+                <img className="md:hidden w-20 h-20 "
+                src={fullLogo}
+                alt="logo completo wave.clear"/>
+                <img className= "hidden md:flex md:pt-3"
+                src={name} 
+                alt="name wave.clear"/>
                 <Navbar />
+                <img className="hidden md:flex md:bottom-0 "
+                src={logo}
+                alt="" />
             </div>
         </>
     );
