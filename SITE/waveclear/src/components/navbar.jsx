d8f180e34287779310e8a84224ecbf93382846e1
dagalera <dagalera.dev@gmail.com>
2024-05-30 19:20:56 -0300

    fix nav on cell

--- SITE/waveclear/src/components/navbar.jsx
+++ SITE/waveclear/src/components/navbar.jsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (  
         <>
-            <ul>
+            <ul className='flex gap-5 mr-4 text-primary-white md:flex md:flex-col md:gap-5 md:mt-5'>
                 <li><Link to='/problema'>Problema</Link></li>
                 <li><Link to='/solucao'>Solucao</Link></li>
                 <li><Link to='/previsao'>Previsão</Link></li>
                 <li><Link to='/'>Home</Link></li>
             </ul>
-            <ul>
+            <ul className='hidden text-primary-white md:flex md:gap-5 md:flex-col md:mt-10 md:pl-[6px] md:h-screen'>
                 <li><Link to='/equipe'>Conheça a equipe</Link></li>
                 <li><Link to='/contato'>Entre em contato</Link></li>
             </ul>
