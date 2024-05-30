9d81efa449062fd601a1f30db76c6114112e7e16
dagalera <dagalera.dev@gmail.com>
2024-05-30 19:28:08 -0300

    parabens fez um codigo decente agr tio

--- SITE/waveclear/src/App.jsx
+++ SITE/waveclear/src/App.jsx
@@ -6,8 +6,8 @@ import { Outlet } from 'react-router-dom'
 export default function App() {
   return (
     <>
-      <div className="flex">
-        <div className="md:flex md:h-screen md:w-1/12 md:max-w-[110px] md:min-w-[110px]">
+      <div className="flex flex-col md:flex-row">
+        <div className=" md:h-screen md:w-1/12 md:max-w-[110px] md:min-w-[110px]">
           <Header />
         </div>
         <div className="md:flex md:m-0 md:h-screen md:w-11/12">
--- SITE/waveclear/src/components/header.jsx
+++ SITE/waveclear/src/components/header.jsx
@@ -5,7 +5,7 @@ import name from '../assets/name.png'
 
 function Header() {
     return (
-        <div className="bg-nav-bg md:h-full md:fixed md:flex md:flex-col md:shadow-md md:w-28">
+        <div className="bg-nav-bg flex justify-between items-center w-screen md:h-full md:fixed md:flex md:flex-col md:shadow-md md:w-28">
             <img className="md:hidden w-20 h-20" src={fullLogo} alt="logo completo wave.clear"/>
             <img className="hidden md:flex md:pt-3" src={name} alt="name wave.clear"/>
             <Navbar />
