703c92517974a49237cc414174a466c1434a8071
dagalera <dagalera.dev@gmail.com>
2024-05-31 22:10:18 -0300

    add google login

--- SITE/waveclear/src/components/navbar.jsx
+++ SITE/waveclear/src/components/navbar.jsx
@@ -1,7 +1,9 @@
 import { Link } from 'react-router-dom';
+import { GoogleLogin } from '@react-oauth/google';
 
 function Navbar() {
-    return (  
+
+    return (
         <>
             <ul className='flex gap-5 mr-4 text-center text-primary-white md:flex md:flex-col md:gap-5 md:mt-5'>
 
@@ -9,7 +11,15 @@ function Navbar() {
                 <li className='ml-3'><Link to='/problema'>PROBLEMA</Link></li>
                 <li className='ml-3'><Link to='/solucao'>SOLUÇÃO</Link></li>
                 <li className='ml-3'><Link to='/previsao'>PREVISÃO</Link></li>
-                
+                <li className='w-10 se '><GoogleLogin
+                    onSuccess={credentialResponse => {
+                        console.log(credentialResponse);
+                    }}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}
+                /></li>
+
             </ul>
             <ul className='hidden text-primary-white md:flex md:gap-5 md:flex-col md:mt-10 md:pl-[6px] md:h-screen'>
                 <li><Link to='/equipe'>Nossa equipe</Link></li>
--- SITE/waveclear/src/main.jsx
+++ SITE/waveclear/src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Contato from './pages/contato.jsx';
 import Equipe from './pages/equipe.jsx';
 import Previsao from './pages/previsao.jsx';
 import Solucao from './pages/solucao.jsx';
@@ -9,7 +10,7 @@ import Problema from './pages/problema.jsx';
 import Home from './pages/home.jsx'
 import PageNotFound from './pages/pageNotFound.jsx';
 import {createBrowserRouter, RouterProvider,Link} from 'react-router-dom'
-import Contato from './pages/contato.jsx';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const router = createBrowserRouter([
   {
@@ -30,7 +31,9 @@ const router = createBrowserRouter([
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <GoogleOAuthProvider clientId="379810967412-1j31nfovrketatj69utkdttn3tomma78.apps.googleusercontent.com">
   <React.StrictMode>
      <RouterProvider router={router}/>  
   </React.StrictMode>,
+  </GoogleOAuthProvider>
 )
