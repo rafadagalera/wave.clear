a2768ef3e3103100427d573f2ff1f79e34941a8f
dagalera <dagalera.dev@gmail.com>
2024-06-03 17:28:18 -0300

    login na home

--- SITE/waveclear/src/components/navbar.jsx
+++ SITE/waveclear/src/components/navbar.jsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { GoogleLogin } from '@react-oauth/google';
+
 
 function Navbar() {
 
--- SITE/waveclear/src/pages/home.jsx
+++ SITE/waveclear/src/pages/home.jsx
@@ -1,4 +1,6 @@
 import { Link } from 'react-router-dom';
+import { GoogleLogin } from '@react-oauth/google';
+
 function Home() {
     const cards = [
         {
@@ -20,26 +22,36 @@ function Home() {
             link: "/previsao"
         }
     ]
-    return (  
+    return (
         <>
-        <section className='bg-white'>   
+            <section className='bg-white'>
+                <div>
+                    <GoogleLogin
+                        onSuccess={credentialResponse => {
+                            console.log(credentialResponse);
+                        }}
+                        onError={() => {
+                            console.log('Login Failed');
+                        }}
+                    />;
+                </div>
 
-            <div className='flex p-10 flex-col gap-4 mt-5 md:w-3/4 md:m-auto'>
-                <h1 className='text-6xl m-auto md:mt-20 text-teal-700'>Nós somos a <b>wave.clear</b></h1>
-                <p className='max-w-3xl m-auto mt-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis, turpis et luctus varius, sapien lacus porttitor sapien, sit amet tempus nisi justo et est. Curabitur pharetra ultricies pulvinar. Cras sit amet dapibus sem, nec hendrerit diam. Nulla eu diam ultrices, placerat ipsum at, posuere ligula.</p>
-            </div>
-            <div className=" p-10 flex flex-col gap-8 md:flex-row md:w-3/4 md:m-auto md:mt-20">
-                {cards.map((card) => (
-                    <div key={card.id} className="group relative box-border border-2 border-primary-white rounded-xl p-5 ">
-                        <h2 className="text-3xl pb-3">{card.title}</h2>
-                        <p className="text-xl ">{card.description}</p>
-                        <button className="flex m-auto mt-4 box-border border-1 border-primary-black bg-teal-700 text-primary-white h-14 w-44 items-center justify-center rounded-md md:mt-20"><Link to={card.link}>Saiba mais</Link></button>
-                    </div>
-                ))}
-            </div>
+                <div className='flex p-10 flex-col gap-4 mt-5 md:w-3/4 md:m-auto'>
+                    <h1 className='text-6xl m-auto md:mt-20 text-teal-700'>Nós somos a <b>wave.clear</b></h1>
+                    <p className='max-w-3xl m-auto mt-6 '>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse iaculis, turpis et luctus varius, sapien lacus porttitor sapien, sit amet tempus nisi justo et est. Curabitur pharetra ultricies pulvinar. Cras sit amet dapibus sem, nec hendrerit diam. Nulla eu diam ultrices, placerat ipsum at, posuere ligula.</p>
+                </div>
+                <div className=" p-10 flex flex-col gap-8 md:flex-row md:w-3/4 md:m-auto md:mt-20">
+                    {cards.map((card) => (
+                        <div key={card.id} className="group relative box-border border-2 border-primary-white rounded-xl p-5 ">
+                            <h2 className="text-3xl pb-3">{card.title}</h2>
+                            <p className="text-xl ">{card.description}</p>
+                            <button className="flex m-auto mt-4 box-border border-1 border-primary-black bg-teal-700 text-primary-white h-14 w-44 items-center justify-center rounded-md md:mt-20"><Link to={card.link}>Saiba mais</Link></button>
+                        </div>
+                    ))}
+                </div>
 
-        </section>
-        
+            </section>
+
         </>
     );
 }
